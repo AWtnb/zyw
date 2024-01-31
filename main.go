111c5d6fba7283c26476f702db956fe79cce91b3
awtnb <arikaebanataw@gmail.com>
2024-01-31 14:03:45 +0900

    fix

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func (cur *CurrentDir) setInfo(curPath string, root string) {
 }
 
 func (cur CurrentDir) configSearch(root string) (searchRoot string, depth int) {
-	if root == "..." {
+	if root == ".." {
 		searchRoot = filepath.Dir(cur.path)
 		depth = 2
 		return
