bdacd4b453b5a41ac7f1b91aaafc88103460268c
awtnb <arikaebanataw@gmail.com>
2024-04-30 11:07:20 +0900

    update

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 Command line tool for [Tablacus Explorer](https://tablacus.github.io/explorer.html).
 
+```
+> .\tablacus-fz-under.exe -h
+
+  -all
+        switch to search including file
+  -cur string
+        current directory
+  -exclude string
+        path to skip searching (comma-separated)
+  -filer string
+        filer (default "explorer.exe")
+  -offset 0
+        Specify the directory to start file traversing, by the number of layers from the current directory.
+        `0` for the current directory, `1` for the parent directory, `2` for its parent directory, and so on.
+        If this value is negative, the path is traversed back to the directory containing the file `.root`. If no `.root` file is found, the current directory is used as the root of the search. (default -1)
+```
+
 - List up subdirectories and select with fuzzy-finder.
 - Open selected directory on new tab.
 
--- main.go
+++ main.go
@@ -14,64 +14,64 @@ import (
 func main() {
 	var (
 		cur     string
-		root    string
+		offset  int
 		filer   string
 		exclude string
 		all     bool
 	)
 	flag.StringVar(&cur, "cur", "", "current directory")
-	flag.StringVar(&root, "root", "", "root directory")
+	flag.IntVar(&offset, "offset", -1, "Specify the directory to start file traversing, by the number of layers from the current directory.\n`0` for the current directory, `1` for the parent directory, `2` for its parent directory, and so on.\nIf this value is negative, the path is traversed back to the directory containing the file `.root`. If no `.root` file is found, the current directory is used as the root of the search.")
 	flag.StringVar(&filer, "filer", "explorer.exe", "filer")
 	flag.StringVar(&exclude, "exclude", "", "path to skip searching (comma-separated)")
 	flag.BoolVar(&all, "all", false, "switch to search including file")
 	flag.Parse()
 	var f Filer
 	f.SetPath(filer)
-	var cd CurrentDir
-	cd.setInfo(cur, root)
+	cd := CurrentDir{Path: cur}
+	if -1 < offset {
+		cd.setRootRel(offset)
+	} else {
+		cd.setRoot()
+	}
 	os.Exit(run(f, cd, exclude, all))
 }
 
 type CurrentDir struct {
-	path       string
-	searchRoot string
-	depth      int
+	Path string
+	root string
 }
 
-func (cur *CurrentDir) setInfo(curPath string, root string) {
-	cur.path = curPath
-	cur.searchRoot, cur.depth = cur.configure(root)
+func (cur CurrentDir) pathElems() []string {
+	return strings.Split(cur.Path, string(os.PathSeparator))
 }
 
-func (cur CurrentDir) configure(root string) (searchRoot string, depth int) {
-	if root == "." {
-		searchRoot = cur.path
-		depth = -1
+func (cur *CurrentDir) setRootRel(offset int) {
+	elems := cur.pathElems()
+	if 0 < offset && offset < len(elems) {
+		ss := elems[0 : len(elems)-offset]
+		cur.root = strings.Join(ss, string(os.PathSeparator))
 		return
 	}
-	if root == ".." {
-		searchRoot = filepath.Dir(cur.path)
-		depth = -1
-		return
-	}
-	elems := strings.Split(cur.path, string(os.PathSeparator))
-	for i := 0; i <= len(elems); i++ {
+	cur.root = cur.Path
+}
+
+func (cur *CurrentDir) setRoot() {
+	elems := cur.pathElems()
+	for i := 0; i < len(elems); i++ {
 		ln := len(elems) - i
 		p := strings.Join(elems[0:ln], string(os.PathSeparator))
-		if filepath.Dir(p) == root {
-			searchRoot = p
-			depth = -1
+		if _, err := os.Stat(filepath.Join(p, ".root")); err == nil {
+			cur.root = p
 			return
 		}
+
 	}
-	searchRoot = cur.path
-	depth = -1
-	return
+	cur.root = cur.Path
 }
 
 func (cur CurrentDir) getChildItemsFromRoot(exclude string, all bool) (assisted bool, found []string, err error) {
-	d := walk.Dir{All: all, Root: cur.searchRoot}
-	d.SetWalkDepth(cur.depth)
+	d := walk.Dir{All: all, Root: cur.root}
+	d.SetWalkDepth(-1)
 	d.SetWalkException(exclude)
 	found, err = d.GetChildItemWithEverything()
 	assisted = true
@@ -84,7 +84,7 @@ func (cur CurrentDir) getChildItemsFromRoot(exclude string, all bool) (assisted 
 
 func (cur CurrentDir) dropCurrent(childPaths []string) (paths []string) {
 	for _, p := range childPaths {
-		if p == cur.path {
+		if p == cur.Path {
 			continue
 		}
 		paths = append(paths, p)
@@ -97,7 +97,7 @@ func (cur CurrentDir) selectItem(childPaths []string, prompt string) (string, er
 		return "", nil
 	}
 	idx, err := fuzzyfinder.Find(childPaths, func(i int) string {
-		rel, _ := filepath.Rel(cur.searchRoot, childPaths[i])
+		rel, _ := filepath.Rel(cur.root, childPaths[i])
 		return rel
 	}, fuzzyfinder.WithPromptString(prompt))
 	if err != nil {
@@ -126,6 +126,6 @@ func run(fl Filer, cur CurrentDir, exclude string, all bool) int {
 	if len(se) < 1 {
 		return 0
 	}
-	fl.OpenSmart(se, cur.path)
+	fl.OpenSmart(se, cur.Path)
 	return 0
 }
