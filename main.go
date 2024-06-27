25f417840c3e400ccb49dfd231bb50b9f9e0a543
awtnb <arikaebanataw@gmail.com>
2024-06-27 13:20:20 +0900

    split files

--- main.go
+++ main.go
@@ -4,11 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/AWtnb/go-walk"
-	"github.com/ktr0731/go-fuzzyfinder"
 )
 
 func main() {
@@ -25,84 +20,14 @@ func main() {
 	flag.StringVar(&exclude, "exclude", "", "path to skip searching (comma-separated)")
 	flag.BoolVar(&all, "all", false, "switch to search including file")
 	flag.Parse()
+
 	var f Filer
 	f.Init(filer)
-	cd := CurrentDir{Path: cur}
-	if -1 < offset {
-		cd.setRootRel(offset)
-	} else {
-		cd.setRoot()
-	}
-	os.Exit(run(f, cd, exclude, all))
-}
 
-type CurrentDir struct {
-	Path string
-	root string
-}
+	var d CurrentDir
+	d.Init(cur, offset)
 
-func (cur CurrentDir) pathElems() []string {
-	return strings.Split(cur.Path, string(os.PathSeparator))
-}
-
-func (cur *CurrentDir) setRootRel(offset int) {
-	elems := cur.pathElems()
-	if 0 < offset && offset < len(elems) {
-		ss := elems[0 : len(elems)-offset]
-		cur.root = strings.Join(ss, string(os.PathSeparator))
-		return
-	}
-	cur.root = cur.Path
-}
-
-func (cur *CurrentDir) setRoot() {
-	elems := cur.pathElems()
-	for i := 0; i < len(elems); i++ {
-		ln := len(elems) - i
-		p := strings.Join(elems[0:ln], string(os.PathSeparator))
-		if _, err := os.Stat(filepath.Join(p, ".root")); err == nil {
-			cur.root = p
-			return
-		}
-
-	}
-	cur.root = cur.Path
-}
-
-func (cur CurrentDir) getChildItemsFromRoot(exclude string, all bool) (assisted bool, found []string, err error) {
-	var d walk.Dir
-	d.Init(cur.root, all, -1, exclude)
-	found, err = d.GetChildItemWithEverything()
-	assisted = true
-	if err != nil || len(found) < 1 {
-		assisted = false
-		found, err = d.GetChildItem()
-	}
-	return
-}
-
-func (cur CurrentDir) dropCurrent(childPaths []string) (paths []string) {
-	for _, p := range childPaths {
-		if p == cur.Path {
-			continue
-		}
-		paths = append(paths, p)
-	}
-	return
-}
-
-func (cur CurrentDir) selectItem(childPaths []string, prompt string) (string, error) {
-	if len(childPaths) < 1 {
-		return "", nil
-	}
-	idx, err := fuzzyfinder.Find(childPaths, func(i int) string {
-		rel, _ := filepath.Rel(cur.root, childPaths[i])
-		return filepath.ToSlash(rel)
-	}, fuzzyfinder.WithPromptString(prompt))
-	if err != nil {
-		return "", err
-	}
-	return childPaths[idx], nil
+	os.Exit(run(f, d, exclude, all))
 }
 
 func run(fl Filer, cur CurrentDir, exclude string, all bool) int {
@@ -125,7 +50,7 @@ func run(fl Filer, cur CurrentDir, exclude string, all bool) int {
 	if len(se) < 1 {
 		return 0
 	}
-	if err := fl.OpenSmart(se, cur.Path); err != nil {
+	if err := fl.OpenSmart(se, cur.Path()); err != nil {
 		fmt.Println(err)
 		return 1
 	}
