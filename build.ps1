0f28ad83775eb481e63c679d9e12719c9f59e898
awtnb <arikaebanataw@gmail.com>
2025-03-20 11:03:05 +0900

    update builder

--- build.ps1
+++ build.ps1
@@ -1,26 +1,14 @@
-go build
-
-if ($LASTEXITCODE -eq 0) {
-    if (Test-Path .\.env) {
-        $d = (Get-Content .\.env -Raw).Trim()
-        $d = [System.Environment]::ExpandEnvironmentVariables($d)
-        if (-not (Test-Path $d -PathType Container)) {
-            New-Item -Path $d -ItemType Directory
-        }
-        $m = (Get-Content "go.mod" | Select-Object -First 1) -replace "^module "
-        $n = "{0}.exe" -f ($m -split "/" | Select-Object -Last 1)
-        if (Test-Path $n) {
-            Get-Item $n | Copy-Item -Destination $d -Force
-            "COPIED {0} to: {1}" -f $n, $d | Write-Host -ForegroundColor Blue
-        }
-        else {
-            "{0} not found!" -f $n | Write-Host -ForegroundColor Magenta
-        }
-    }
-    else {
-        ".env not found!" | Write-Host -ForegroundColor Magenta
-    }
+$d = $args[0]
+if ($d.length -lt 1) {
+    Write-Host "Specify directory to copy executable."
 }
 else {
-    "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
+    go build -o $d
+
+    if ($LASTEXITCODE -eq 0) {
+        "[FINISHED] Executable was built on {0}" -f $d | Write-Host -ForegroundColor Blue
+    }
+    else {
+        "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
+    }
 }
