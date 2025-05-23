7a79c2183c19af1d94a31726e76322fc0c0f6a38
awtnb <arikaebanataw@gmail.com>
2025-05-23 09:54:32 +0900

    simpify

--- README.md
+++ README.md
@@ -8,14 +8,10 @@ Traverse directory and select path with fuzzy-finder.
 
   -all
         switch to search including file
-  -src string
-        source directory
   -exclude string
-        path to skip searching (comma-separated)
-  -offset 0
-        Specify the directory to start file traversal, by the number of layers from the current directory.
-        `0` for the current directory, `1` for the parent directory, `2` for its parent directory, and so on.
-        If this value is negative, the path is traversed back to the directory containing the file `.root`. If no `.root` file is found, the current directory is used as the root of the search. (default -1)
+        directory name to skip (comma-separated)
+  -root string
+        root of traversal
 ```
 
 The directory where the [`.root`](.root) is placed should be the starting point of the traversal.
--- main.go
+++ main.go
@@ -4,30 +4,57 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/AWtnb/go-walk"
 	"github.com/ktr0731/go-fuzzyfinder"
 )
 
-func main() {
-	var (
-		src     string
-		offset  int
-		all     bool
-		exclude string
-	)
-	flag.StringVar(&src, "src", "", "source directory")
-	flag.IntVar(&offset, "offset", -1, "Specify the directory to start file traversal, by the number of layers from the current directory.\n`0` for the current directory, `1` for the parent directory, `2` for its parent directory, and so on.\nIf this value is negative, the path is traversed back to the directory containing the file `.root`. If no `.root` file is found, the current directory is used as the root of the search.")
-	flag.BoolVar(&all, "all", false, "switch to search including file")
-	flag.StringVar(&exclude, "exclude", "", "path to skip searching (comma-separated)")
-	flag.Parse()
-
-	os.Exit(run(src, offset, exclude, all))
+type WalkRoot struct {
+	path    string
+	exclude string
+	all     bool
 }
 
-func run(src string, offset int, exclude string, all bool) int {
-	var sd SrcDir
-	sd.Init(src, offset, exclude, all)
-	p, err := sd.SelectItem()
+func (wr *WalkRoot) Init(path string, exclude string, all bool) {
+	wr.path = path
+	wr.exclude = exclude
+	wr.all = all
+}
+
+func (wr WalkRoot) walk() (prompt string, found []string, err error) {
+	var d walk.Dir
+	d.Init(wr.path, wr.all, -1, wr.exclude)
+	found, err = d.GetChildItemWithEverything()
+	if err != nil || len(found) < 1 {
+		prompt = ">"
+		found, err = d.GetChildItem()
+	} else {
+		prompt = "#"
+	}
+	return
+}
+
+func (wr WalkRoot) SelectItem() (string, error) {
+	prompt, childPaths, err := wr.walk()
+	if err != nil || len(childPaths) < 1 {
+		return "", nil
+	}
+
+	idx, err := fuzzyfinder.Find(childPaths, func(i int) string {
+		rel, _ := filepath.Rel(wr.path, childPaths[i])
+		return filepath.ToSlash(rel)
+	}, fuzzyfinder.WithPromptString(prompt))
+	if err != nil {
+		return "", err
+	}
+	return childPaths[idx], nil
+}
+
+func run(root string, exclude string, all bool) int {
+	var r WalkRoot
+	r.Init(root, exclude, all)
+	p, err := r.SelectItem()
 	if err != nil {
 		if err != fuzzyfinder.ErrAbort {
 			fmt.Println(err.Error())
@@ -38,4 +65,18 @@ func run(src string, offset int, exclude string, all bool) int {
 		fmt.Print(p)
 	}
 	return 0
+}
+
+func main() {
+	var (
+		root    string
+		all     bool
+		exclude string
+	)
+	flag.StringVar(&root, "root", "", "root of traversal")
+	flag.BoolVar(&all, "all", false, "switch to search including file")
+	flag.StringVar(&exclude, "exclude", "", "directory name to skip (comma-separated)")
+	flag.Parse()
+
+	os.Exit(run(root, exclude, all))
 }
